3fe909950de7c520bc9066c70cb2f49e66d4278a
daniel <copam7171@gmail.com>
2022-01-19 17:10:51 +0800

    add get & update rtsp config add record CURD

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from typing import Optional
-from fastapi import FastAPI
+from datetime import datetime as dt
+from uuid import UUID
+from fastapi import FastAPI, HTTPException
 from fastapi.openapi.utils import get_openapi
+from db_postgres import Database
 
 APP_VERSION = "0.1.0"
-app = FastAPI()  # 建立一個 Fast API application
+# 建立一個 Fast API application
+app = FastAPI()
+# create postgres instance
+db = Database()
 
 
 def custom_openapi():
@@ -12,7 +18,7 @@ def custom_openapi():
     openapi_schema = get_openapi(
         title="大鵬灣車流&人流計數系統",
         version=APP_VERSION,
-        description="此文件說明如何透過Restful API存取車流&人流計數的統計資料",
+        description="此文件說明如何透過Restful API存取車流&人流計數的統計資料與RTSP相關設定值的設定與存取",
         routes=app.routes,
     )
     openapi_schema["info"]["x-logo"] = {
@@ -26,26 +32,113 @@ def custom_openapi():
 app.openapi = custom_openapi
 
 
-@app.get("/")  # 回傳此服務的版本號碼
+@app.get("/")
 def check_health():
+    '''
+    回傳此服務的版本號碼
+    '''
     return "The Service works fine, Version:{}".format(APP_VERSION)
 
 
-@app.get("/check_nvr")  # 檢查與NVR影像主機間的通訊是否正常
+@app.get("/check_nvr")
 def check_nvr():
-    return {"Status": "OK"}
+    '''
+    檢查與NVR影像主機間的通訊是否正常
+    '''
+    return {"status": "OK"}
 
 
-@app.get("/check_ai_model")  # 檢查與NVR影像主機間的通訊是否正常
+@app.get("/check_ai_model")
 def check_ai_model():
-    return {"Status": "OK"}
+    '''
+    檢查AI model是否正常
+    '''
+    return {"status": "OK"}
 
 
-@app.get("/object_counter")  # 指定影像來源、日期與時間參數下的物件辨識統計資料
-def get_object_counter(cam_id: str, date: str, start_time: str, end_time: str):
-    print(cam_id, date, start_time, end_time)
-    data = {
-        "Big Car": 10,
-        "Small Car": 50
-    }
-    return data
+@app.get("/rtsp_config")
+def get_rtsp_config():
+    '''
+    取得連結NVR的設定值,帳號,密碼,連線位址,port number
+    '''
+    data = {}
+
+    try:
+        data = db.get_config()
+    except Exception as err:
+        raise HTTPException(status_code=500, detail=str(err))
+    else:
+        return data
+
+
+@app.patch("/rtsp_config")
+def update_config(account: Optional[str] = None, password: Optional[str] = None, host: Optional[str] = None, port: Optional[int] = None):
+    '''
+    更新連結NVR的設定值
+    '''
+    config = {}
+    try:
+        config = db.update_config(account, password, host, port)
+    except Exception as err:
+        raise HTTPException(status_code=500, detail=str(err))
+    else:
+        return {"status": "OK", 'config': config}
+
+
+@app.get("/records")
+def get_records(id: Optional[UUID] = None, cam_id: Optional[str] = None, start_time: Optional[dt] = None, end_time: Optional[dt] = None, type: Optional[str] = None):
+    '''
+    取得指定影像來源、日期與時間參數下的物件辨識統計資料\n
+    沒有設定參數時取得全部資料\n
+    有設定時間時,start_time 與 end_time皆須設定\n
+    start_time & end_time format are "%Y-%m-%d %H:%M:%S".\n
+    e.g.: 2022-01-19 08:00:00
+    '''
+    data = []
+    if start_time and not end_time:
+        raise HTTPException(
+            status_code=422, detail="end_time could not be empty")
+    if end_time and not start_time:
+        raise HTTPException(
+            status_code=422, detail="start_time could not be empty")
+    if start_time > end_time:
+        raise HTTPException(
+            status_code=422, detail="end_time should be less than start_time")
+    try:
+        if not id and not cam_id and not start_time and not end_time and not type:
+            data = db.get_all_records()
+        else:
+            data = db.get_record(id, cam_id, start_time, end_time, type)
+    except Exception as err:
+        raise HTTPException(status_code=500, detail=str(err))
+    else:
+        return data
+
+
+@app.post("/record")
+def add_record(camId: str, time: dt, type: str, inValue: int, outValue: int):
+    '''
+    新增一筆影像辨識紀錄\n
+    time format is "%Y-%m-%d %H:%M:%S".\n
+    e.g.: 2022-01-19 08:00:00
+    '''
+    try:
+        db.add_record(camId, time, type, inValue, outValue)
+    except Exception as err:
+        raise HTTPException(status_code=500, detail=str(err))
+    else:
+        return {"status": "OK"}
+
+
+@app.delete("/record")
+def delect_record(id: UUID):
+    '''
+    刪除一筆影像紀錄\n
+    id為該筆紀錄的uuid
+    '''
+    try:
+        db.delete_record(id)
+    except Exception as err:
+        raise HTTPException(status_code=500, detail=str(err))
+    else:
+        return {"status": "OK"}
--- db_postgres.py
+++ db_postgres.py
@@ -1,55 +1,185 @@
 import psycopg2
+from datetime import datetime as dt
 
 HOST = 'localhost'
 DB_NAME = 'dapeng'
-TABLE_NAME = 'object_counter'
+RECORD_TABLE_NAME = 'record'
+CONFIG_TABLE_NAME = 'config'
 USER = 'postgres'
 PWD = 'postgres'
 SSL_MODE = 'require'
 
-db_conn = None
 
+class Database:
+    def __init__(self) -> None:
+        '''
+        initialize database connection and make sure the object counter table exists
+        '''
+        # create connection
+        self.conn = psycopg2.connect(database=DB_NAME, user=USER,
+                                     password=PWD, host=HOST)
+        # set auto commit is true
+        self.conn.autocommit = True
+        # get instance of cursor
+        self.cursor = self.conn.cursor()
+        # check version
+        self.cursor.execute('SELECT version()')
+        result = self.cursor.fetchall()
+        print("Opened database successfully! Version:{}".format(result[0]))
+        # install uuid-ossp extension
+        self.cursor.execute('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
+        # check object counter table existed or not
+        checkTableExisted = "SELECT EXISTS ( \
+                SELECT FROM pg_tables \
+                WHERE  schemaname = 'public' \
+                AND    tablename  = '{}');".format(RECORD_TABLE_NAME)
+        self.cursor.execute(checkTableExisted)
+        result = self.cursor.fetchall()
+        # if table doesn't exist create that
+        if False in result[0]:
+            createTable = "CREATE TABLE IF NOT EXISTS {} ( \
+                id uuid DEFAULT uuid_generate_v4 (), \
+                camId VARCHAR(20) NOT NULL, \
+                time timestamp NOT NULL, \
+                type VARCHAR(20) NOT NULL, \
+                inCounter int NOT NULL, \
+                outCounter int NOT NULL, \
+                PRIMARY KEY (id) \
+                );".format(RECORD_TABLE_NAME)
+            self.cursor.execute(createTable)
+            self.conn.commit()
+            print("{} created successfully".format(RECORD_TABLE_NAME))
+        else:
+            print("Table '{}' is existed!".format(RECORD_TABLE_NAME))
+        # check account table existed or not
+        checkTableExisted = "SELECT EXISTS ( \
+                SELECT FROM pg_tables \
+                WHERE  schemaname = 'public' \
+                AND    tablename  = '{}');".format(CONFIG_TABLE_NAME)
+        self.cursor.execute(checkTableExisted)
+        result = self.cursor.fetchall()
+        # if table doesn't exist create that
+        if False in result[0]:
+            createTable = "CREATE TABLE IF NOT EXISTS {} ( \
+                id int NOT NULL, \
+                account VARCHAR(20) NOT NULL, \
+                password VARCHAR(20) NOT NULL, \
+                host VARCHAR(50), \
+                port int, \
+                PRIMARY KEY (id) \
+                );".format(CONFIG_TABLE_NAME)
+            self.cursor.execute(createTable)
+            self.conn.commit()
+            print("{} created successfully".format(CONFIG_TABLE_NAME))
+            insertData = "INSERT INTO {} (id,account,password,host,port) \
+                VALUES (0, '{}', '{}', '{}', {});".format(CONFIG_TABLE_NAME, "root", "root", "localhost", 80)
 
-def init():
-    '''
-    initialize database connection and make sure the object counter table exists
-    '''
-    # create connection
-    db_conn = psycopg2.connect(database=DB_NAME, user=USER,
-                               password=PWD, host=HOST)
-    # set auto commit is true
-    db_conn.autocommit = True
-    # get instance of cursor
-    cursor = db_conn.cursor()
-    # check version
-    cursor.execute('SELECT version()')
-    result = cursor.fetchall()
-    print("Opened database successfully! Version:{}".format(result[0]))
-    # install uuid-ossp extension
-    cursor.execute('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
-    # check table existed
-    checkTableExisted = "SELECT EXISTS ( \
-            SELECT FROM pg_tables \
-            WHERE  schemaname = 'public' \
-            AND    tablename  = '{}');".format(TABLE_NAME)
-    cursor.execute(checkTableExisted)
-    result = cursor.fetchall()
-    # if table doesn't exist create that
-    if False in result[0]:
-        createTable = "CREATE TABLE IF NOT EXISTS {} ( \
-            id uuid DEFAULT uuid_generate_v4 (), \
-            camId VARCHAR(20) NOT NULL, \
-            time timestamp NOT NULL, \
-            type VARCHAR(20) NOT NULL, \
-            inCounter int NOT NULL, \
-            outCounter int NOT NULL, \
-            PRIMARY KEY (id) \
-            )".format(TABLE_NAME)
-        cursor.execute(createTable)
-        db_conn.commit()
-        print("{} created successfully".format(TABLE_NAME))
-    else:
-        print("{} is existed!".format(TABLE_NAME))
+            self.cursor.execute(insertData)
+            self.conn.commit()
+            print("insert default config successfully")
+        else:
+            print("Table '{}' is existed!".format(CONFIG_TABLE_NAME))
 
+    def add_record(self, camId: str, time: dt, type: str, inValue: int, outValue: int):
+        insertData = "INSERT INTO {} (camId,time,type,inCounter,outCounter) \
+        VALUES ('{}', '{}', '{}', {}, {});".format(RECORD_TABLE_NAME, camId, time, type, inValue, outValue)
 
-init()
+        self.cursor.execute(insertData)
+        self.conn.commit()
+
+        print("insert record successfully")
+
+    def get_all_records(self):
+        data = []
+        getAllData = "SELECT * FROM {};".format(RECORD_TABLE_NAME)
+
+        self.cursor.execute(getAllData)
+        rows = self.cursor.fetchall()
+        for row in rows:
+            record = {'id': row[0], 'camId': row[1], 'time': row[2],
+                      'type': row[3], 'inCounter': row[4], 'outCounter': row[5]}
+            data.append(record)
+
+        return data
+
+    def get_record(self, id=None, camId=None, start_time=None, end_time=None, type=None):
+        data = []
+        query = ''
+        if id:
+            query += "id='{}' ".format(id)
+        if camId:
+            if query:
+                query += 'AND '
+            query += "camId='{}' ".format(camId)
+        if start_time and end_time:
+            if query:
+                query += 'AND '
+            query += "time >= '{}' AND time <= '{}'".format(
+                start_time, end_time)
+        if type:
+            if query:
+                query += 'AND '
+            query += "type='{}' ".format(type)
+
+        query = "SELECT * FROM {} WHERE ".format(RECORD_TABLE_NAME) + query
+        print("Get record Query:", query)
+        self.cursor.execute(query)
+        rows = self.cursor.fetchall()
+        for row in rows:
+            record = {'id': row[0], 'camId': row[1], 'time': row[2],
+                      'type': row[3], 'inCounter': row[4], 'outCounter': row[5]}
+            data.append(record)
+
+        return data
+
+    def delete_record(self, id: str):
+        deleteData = "DELETE FROM {} WHERE id='{}';".format(
+            RECORD_TABLE_NAME, id)
+
+        self.cursor.execute(deleteData)
+        self.conn.commit()
+
+        print("delete record successfully")
+
+    def get_config(self):
+        data = {}
+        getAllData = "SELECT * FROM {};".format(CONFIG_TABLE_NAME)
+
+        self.cursor.execute(getAllData)
+        rows = self.cursor.fetchall()
+        print(rows[0])
+        if len(rows) == 1:
+            data['account'] = rows[0][1]
+            data['password'] = rows[0][2]
+            data['host'] = rows[0][3]
+            data['port'] = rows[0][4]
+
+        return data
+
+    def update_config(self, account: str, password: str, host: str, port: int):
+        updateData = ''
+        if account:
+            updateData += "account = '{}'".format(account)
+        if password:
+            if updateData:
+                updateData += ','
+            updateData += "password = '{}'".format(password)
+        if host:
+            if updateData:
+                updateData += ','
+            updateData += "host = '{}'".format(host)
+        if port:
+            if updateData:
+                updateData += ','
+            updateData += "port = {}".format(port)
+        if updateData != '':
+            updateData = "UPDATE {} SET ".format(CONFIG_TABLE_NAME) + updateData + \
+                "WHERE id = 0;"
+
+            self.cursor.execute(updateData)
+            self.conn.commit()
+
+            print("update config successfully")
+        data = self.get_config()
+
+        return data
