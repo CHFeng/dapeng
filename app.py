f87d53888f01d33adc14b2e0b70757aac936458d
daniel <copam7171@gmail.com>
2022-01-20 16:53:37 +0800

    update method

--- app.py
+++ app.py
@@ -41,21 +41,32 @@ def check_health():
     '''
     檢查相關服務狀態\n
     version: 此服務的版本號碼\n
-    nvrStatus: 200(http通訊正常)\n
+    nvrStatus: 從NVR主機回傳的Http回應狀態碼\n
     nvrHosts: NVR主機名稱\n
     '''
+
     data = {
         'version': APP_VERSION,
         'nvrStatus': None,
-        'nvrHosts': None
+        'nvrHosts': []
     }
-    # get nvr config
-    config = db.get_config()
-    url = "http://{}:{}/hosts/".format(config['host'], config['port'])
-    # get domain hosts
-    result = requests.get(url, auth=(config['account'], config['password']))
-    data["nvrStatus"] = result.status_code
-    data["nvrHosts"] = json.loads(result.text)
+    try:
+        # get nvr config
+        config = db.get_config()
+    except Exception as err:
+        raise HTTPException(status_code=500, detail=str(err))
+
+    try:
+        url = "http://{}:{}/hosts/".format(config['host'], config['port'])
+        # get domain hosts
+        result = requests.get(url, auth=(
+            config['account'], config['password']))
+        data["nvrStatus"] = result.status_code
+        if result.status_code == requests.codes.ok:
+            data["nvrHosts"] = json.loads(result.text)
+    except Exception as err:
+        data["nvrStatus"] = 500
+        data["detail"] = str(err)
 
     return data
 
@@ -63,52 +74,65 @@ def check_health():
 @app.get("/check_nvr")
 def check_nvr():
     '''
-    檢查NVR影像來源狀態
+    檢查NVR影像來源狀態\n
+    nvrStatus: 從NVR主機回傳的Http回應狀態碼\n
+    resp
     '''
-    # get nvr config
-    config = db.get_config()
-    url = "http://{}:{}/video-origins/".format(config['host'], config['port'])
-    # get video source status
-    result = requests.get(url, auth=(config['account'], config['password']))
 
-    return {"status": result.status_code, 'msg': json.loads(result.text)}
+    data = {
+        'nvrStatus': None,
+        'resp': {}
+    }
+    try:
+        # get nvr config
+        config = db.get_config()
+    except Exception as err:
+        raise HTTPException(status_code=500, detail=str(err))
 
+    try:
+        url = "http://{}:{}/video-origins/".format(
+            config['host'], config['port'])
+        # get video source status
+        result = requests.get(url, auth=(
+            config['account'], config['password']))
+        data["nvrStatus"] = result.status_code
+        if result.status_code == requests.codes.ok:
+            data["resp"] = json.loads(result.text)
+    except Exception as err:
+        data["nvrStatus"] = 500
+        data["detail"] = str(err)
 
-@app.get("/check_ai_model")
-def check_ai_model():
-    '''
-    檢查AI model是否正常
-    '''
-    return {"status": "OK"}
+    return data
 
 
 @app.get("/rtsp_config")
 def get_rtsp_config():
     '''
-    取得連結NVR的設定值,帳號,密碼,連線位址,port number
+    取得NVR主機的設定值,帳號,密碼,連線位址,PORT
     '''
-    data = {}
 
+    config = {}
     try:
-        data = db.get_config()
+        config = db.get_config()
     except Exception as err:
         raise HTTPException(status_code=500, detail=str(err))
     else:
-        return data
+        return config
 
 
 @app.patch("/rtsp_config")
 def update_config(account: Optional[str] = None, password: Optional[str] = None, host: Optional[str] = None, port: Optional[int] = None):
     '''
-    更新連結NVR的設定值
+    更新NVR主機的設定值
     '''
+
     config = {}
     try:
         config = db.update_config(account, password, host, port)
     except Exception as err:
         raise HTTPException(status_code=500, detail=str(err))
     else:
-        return {"status": "OK", 'config': config}
+        return config
 
 
 @app.get("/records")
@@ -118,9 +142,16 @@ def get_records(id: Optional[UUID] = None, cam_id: Optional[str] = None, start_t
     沒有設定參數時取得全部資料\n
     有設定時間時,start_time 與 end_time皆須設定\n
     start_time & end_time format are "%Y-%m-%d %H:%M:%S".\n
-    e.g.: 2022-01-19 08:00:00
+    e.g.: 2022-01-19 08:00:00 \n
+    Response Value:\n
+    statistics: 每個影像來源的統計數據\n
+    records: 指定條件下的每筆紀錄\n
     '''
-    data = []
+
+    data = {
+        'statistics': {},
+        'records': []
+    }
     if start_time and not end_time:
         raise HTTPException(
             status_code=422, detail="end_time could not be empty")
@@ -132,9 +163,29 @@ def get_records(id: Optional[UUID] = None, cam_id: Optional[str] = None, start_t
             status_code=422, detail="start_time should be less than end_time")
     try:
         if not id and not cam_id and not start_time and not end_time and not type:
-            data = db.get_all_records()
+            rows = db.get_all_records()
         else:
-            data = db.get_record(id, cam_id, start_time, end_time, type)
+            rows = db.get_record(id, cam_id, start_time, end_time, type)
+
+        data['records'] = rows
+        # 統計物件計數數值
+        statistics = {}
+        for row in rows:
+            key = row["camId"]
+            type = row["type"]
+            # 判斷此型態的物件是否存在
+            if key not in statistics:
+                statistics[key] = {}
+                statistics[key] = {}
+            if type not in statistics[key]:
+                statistics[key][type] = {'inCounter': 0, 'outCounter': 0}
+
+            # 累加數據
+            statistics[key][type]['inCounter'] += row['inCounter']
+            statistics[key][type]['outCounter'] += row['outCounter']
+
+        data["statistics"] = statistics
+
     except Exception as err:
         raise HTTPException(status_code=500, detail=str(err))
     else:
@@ -148,12 +199,13 @@ def add_record(camId: str, time: dt, type: str, inValue: int, outValue: int):
     time format is "%Y-%m-%d %H:%M:%S".\n
     e.g.: 2022-01-19 08:00:00
     '''
+
     try:
         db.add_record(camId, time, type, inValue, outValue)
     except Exception as err:
         raise HTTPException(status_code=500, detail=str(err))
     else:
-        return {"status": "OK"}
+        return {"detail": "OK"}
 
 
 @app.delete("/record")
@@ -162,9 +214,10 @@ def delect_record(id: UUID):
     刪除一筆影像紀錄\n
     id為該筆紀錄的uuid
     '''
+
     try:
         db.delete_record(id)
     except Exception as err:
         raise HTTPException(status_code=500, detail=str(err))
     else:
-        return {"status": "OK"}
+        return {"detail": "OK"}
