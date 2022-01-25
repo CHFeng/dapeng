5b6a3c84795123896bdf58e8bf9054733a50d811
daniel.feng <danielfeng@iii.org.tw>
2022-01-25 21:03:30 +0800

    update multi tracker

--- ai_model/detect.py
+++ ai_model/detect.py
@@ -1,18 +1,147 @@
 import cv2
+import tensorflow as tf
 import threading
 from datetime import datetime as dt
+import numpy as np
+import matplotlib.pyplot as plt
 # local import
-# from tools import generate_detections as gdet
+from tools import generate_detections as gdet
+from core.config import cfg
+import core.utils as utils
 # # deep sort imports
-# from deep_sort.tracker import Tracker
-# from deep_sort.detection import Detection
-# from deep_sort import preprocessing, nn_matching
+from deep_sort.tracker import Tracker
+from deep_sort.detection import Detection
+from deep_sort import preprocessing, nn_matching
 
 
-def grab_img(cam):
-    """This 'grab_img' function is designed to be run in the sub-thread.
+def yolov4(cam):
+    nms_max_overlap = 1.0
+    input_size = cam.args.size
+    image_data = cv2.resize(cam.img_handle, (input_size, input_size))
+    image_data = image_data / 255.0
+    image_data = image_data[np.newaxis, ...].astype(np.float32)
+
+    # run detections
+    batch_data = tf.constant(image_data)
+    pred_bbox = cam.infer(batch_data)
+    for key, value in pred_bbox.items():
+        boxes = value[:, :, 0:4]
+        pred_conf = value[:, :, 4:]
+    # combined_non_max_suppression
+    (
+        boxes,
+        scores,
+        classes,
+        valid_detections,
+    ) = tf.image.combined_non_max_suppression(
+        boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
+        scores=tf.reshape(
+            pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
+        max_output_size_per_class=50,
+        max_total_size=50,
+        iou_threshold=cam.args.iou,
+        score_threshold=cam.args.score,
+    )
+    # convert data to numpy arrays and slice out unused elements
+    num_objects = valid_detections.numpy()[0]
+    bboxes = boxes.numpy()[0]
+    bboxes = bboxes[0:int(num_objects)]
+    scores = scores.numpy()[0]
+    scores = scores[0:int(num_objects)]
+    classes = classes.numpy()[0]
+    classes = classes[0:int(num_objects)]
+
+    # format bounding boxes from normalized ymin, xmin, ymax, xmax ---> xmin, ymin, width, height
+    original_h, original_w, _ = cam.img_handle.shape
+    bboxes = utils.format_boxes(bboxes, original_h, original_w)
+
+    # store all predictions in one parameter for simplicity when calling functions
+    pred_bbox = [bboxes, scores, classes, num_objects]
+
+    # loop through objects and use class index to get class name, allow only classes in allowed_classes list
+    names = []
+    deleted_indx = []
+    for i in range(num_objects):
+        class_indx = int(classes[i])
+        class_name = cam.class_names[class_indx]
+        if class_name not in cam.allowed_classes:
+            deleted_indx.append(i)
+        else:
+            names.append(class_name)
+    names = np.array(names)
+    counter = len(names)
+    # delete detections that are not in allowed_classes
+    bboxes = np.delete(bboxes, deleted_indx, axis=0)
+    scores = np.delete(scores, deleted_indx, axis=0)
+    # encode yolo detections and feed to tracker
+    features = cam.encoder(cam.img_handle, bboxes)
+    detections = [
+        Detection(bbox, score, class_name,
+                  feature) for bbox, score, class_name, feature in zip(
+                      bboxes, scores, names, features)
+    ]
+    # run non-maxima supression
+    boxs = np.array([d.tlwh for d in detections])
+    scores = np.array([d.confidence for d in detections])
+    classes = np.array([d.class_name for d in detections])
+    indices = preprocessing.non_max_suppression(boxs, classes, nms_max_overlap,
+                                                scores)
+    detections = [detections[i] for i in indices]
+
+    return detections
+
+
+def deep_sort(cam, detections):
+    # Call the tracker
+    cam.tracker.predict()
+    cam.tracker.update(detections)
+    # initialize color map
+    cmap = plt.get_cmap("tab20b")
+    colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
+    # update tracks
+    for track in cam.tracker.tracks:
+        if not track.is_confirmed() or track.time_since_update > 1:
+            continue
+        bbox = track.to_tlbr()
+        class_name = track.get_class()
+
+        # draw bbox on screen
+        color = colors[int(track.track_id) % len(colors)]
+        color = [i * 255 for i in color]
+        cv2.rectangle(
+            cam.img_handle,
+            (int(bbox[0]), int(bbox[1])),
+            (int(bbox[2]), int(bbox[3])),
+            color,
+            2,
+        )
+        cv2.rectangle(
+            cam.img_handle,
+            (int(bbox[0]), int(bbox[1] - 30)),
+            (
+                int(bbox[0]) +
+                (len(class_name) + len(str(track.track_id))) * 17,
+                int(bbox[1]),
+            ),
+            color,
+            -1,
+        )
+        cv2.putText(
+            cam.img_handle,
+            class_name + "-" + str(track.track_id),
+            (int(bbox[0]), int(bbox[1] - 10)),
+            0,
+            0.75,
+            (255, 255, 255),
+            2,
+        )
+
+
+def sub_process(cam):
+    """This 'sub_process' function is designed to be run in the sub-thread.
     Once started, this thread continues to grab a new image and put it
     into the global 'img_handle', until 'thread_running' is set to False.
+    then execute object detector(yolov4) and tracker(deep sort)
     """
     while cam.thread_running and cam.vid.isOpened():
         _, cam.img_handle = cam.vid.read()
@@ -21,6 +150,10 @@ def grab_img(cam):
             cam.vid.release()
             cam.vid = cv2.VideoCapture(cam.rtspUrl)
             continue
+        # run detection by yolov4
+        detections = yolov4(cam)
+        deep_sort(cam, detections)
+
     cam.thread_running = False
 
 
@@ -29,15 +162,14 @@ class Detect:
         # Definition of the parameters
         max_cosine_distance = 0.4
         nn_budget = None
-        nms_max_overlap = 1.0
-        # # initialize deep sort
-        # model_filename = "model_data/mars-small128.pb"
-        # self.encoder = gdet.create_box_encoder(model_filename, batch_size=1)
-        # # calculate cosine distance metric
-        # metric = nn_matching.NearestNeighborDistanceMetric(
-        #     "cosine", max_cosine_distance, nn_budget)
-        # # initialize tracker
-        # self.tracker = Tracker(metric)
+        # initialize deep sort
+        model_filename = "model_data/mars-small128.pb"
+        self.encoder = gdet.create_box_encoder(model_filename, batch_size=1)
+        # calculate cosine distance metric
+        metric = nn_matching.NearestNeighborDistanceMetric(
+            "cosine", max_cosine_distance, nn_budget)
+        # initialize tracker
+        self.tracker = Tracker(metric, max_age=30)
         # initialize yolov4
         self.infer = infer
         # initialize rtsp url
@@ -48,6 +180,13 @@ class Detect:
         self.vid = None
         self.width = 0
         self.height = 0
+        # read in all class names from config
+        self.class_names = utils.read_class_names(cfg.YOLO.CLASSES)
+        if args.allow_classes:
+            self.allowed_classes = args.allow_classes.split(",")
+        else:
+            # by default allow all classes in .names file
+            self.allowed_classes = list(self.class_names.values())
         self.frame_num = 0
         self.detect_objs = []
         self.lastWriteTime = dt.now()
@@ -85,13 +224,14 @@ class Detect:
         # i.e. rtsp, usb or onboard
         assert not self.thread_running
         self.thread_running = True
-        self.thread = threading.Thread(target=grab_img, args=(self, ))
+        self.thread = threading.Thread(target=sub_process, args=(self, ))
         self.thread.start()
 
     def _stop(self):
         if self.thread_running:
             self.thread_running = False
-            #self.thread.join()
+            print('Wait until Thread is terminating')
+            self.thread.join()
 
     def read(self):
         """Read a frame from the camera object.
--- ai_model/multi_tracker.py
+++ ai_model/multi_tracker.py
@@ -1,33 +1,83 @@
+import os
+import tensorflow as tf
+# comment out below line to enable tensorflow logging outputs
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = "3"
+
+physical_devices = tf.config.experimental.list_physical_devices("GPU")
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
 import cv2
 from detect import Detect
+from absl import app, flags, logging
+from absl.flags import FLAGS
+# tensoflow
+from tensorflow.compat.v1 import InteractiveSession
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.python.saved_model import tag_constants
+# local import
+import core.utils as utils
 
-rtspUrl = "rtsp://user1:user10824@60.249.33.163:554/hosts/DESKTOP-F093S18/DeviceIpint.103/SourceEndpoint.video:0:0"
-rtspUrl2 = "rtsp://user1:user10824@60.249.33.163:554/hosts/DESKTOP-F093S18/DeviceIpint.101/SourceEndpoint.video:0:0"
-cam = Detect(rtspUrl, None, None)
-cam2 = Detect(rtspUrl2, None, None)
+flags.DEFINE_string("weights", "./checkpoints/yolov4-416",
+                    "path to weights file")
+flags.DEFINE_integer("size", 416, "resize images to")
+flags.DEFINE_boolean("tiny", False, "yolo or yolo-tiny")
+flags.DEFINE_string("model", "yolov4", "yolov3 or yolov4")
+flags.DEFINE_float("iou", 0.45, "iou threshold")
+flags.DEFINE_float("score", 0.50, "score threshold")
+flags.DEFINE_boolean("dont_show", False, "dont show video output")
+flags.DEFINE_boolean("info", True, "show detailed info of tracked objects")
+flags.DEFINE_string("allow_classes", "person,car,truck,bus,motorbike",
+                    "allowed classes")
 
-camIdx = 0
-while True:
-    if camIdx == 0:
-        img = cam.read()
-    else:
-        img = cam2.read()
 
-    img = cv2.resize(img, (1280, 720))
-    cv2.imshow('result', img)
+def main(_argv):
+    # load configuration for object detector
+    config = ConfigProto()
+    config.gpu_options.allow_growth = True
+    InteractiveSession(config=config)
+    STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
+    saved_model_loaded = tf.saved_model.load(FLAGS.weights,
+                                             tags=[tag_constants.SERVING])
+    infer = saved_model_loaded.signatures['serving_default']
 
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord("q"):
-        break
-    elif key == ord("0"):
-        camIdx = 0
-    elif key == ord("1"):
-        camIdx = 1
-cam.release()
-cam2.release()
-while True:
-    if cam.isOpened() == False and cam2.isOpened() == False:
-        break
-    else:
-        print("wait thread release")
-cv2.destroyAllWindows()
+    rtspUrl = "rtsp://user1:user10824@60.249.33.163:554/hosts/DESKTOP-F093S18/DeviceIpint.103/SourceEndpoint.video:0:0"
+    rtspUrl2 = "rtsp://user1:user10824@60.249.33.163:554/hosts/DESKTOP-F093S18/DeviceIpint.101/SourceEndpoint.video:0:0"
+    rtspUrl3 = "rtsp://user1:user10824@60.249.33.163:554/hosts/DESKTOP-F093S18/DeviceIpint.102/SourceEndpoint.video:0:0"
+    cam = Detect(rtspUrl, infer, FLAGS)
+    cam2 = Detect(rtspUrl2, infer, FLAGS)
+    cam3 = Detect(rtspUrl3, infer, FLAGS)
+
+    camIdx = 0
+    while True:
+        if camIdx == 0:
+            img = cam.read()
+        elif camIdx == 1:
+            img = cam2.read()
+        else:
+            img = cam3.read()
+
+        if img is not None:
+            img = cv2.resize(img, (1280, 720))
+            cv2.imshow('result', img)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
+        elif key == ord("0"):
+            camIdx = 0
+        elif key == ord("1"):
+            camIdx = 1
+        elif key == ord("2"):
+            camIdx = 2
+    cam.release()
+    cam2.release()
+    cam3.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    try:
+        app.run(main)
+    except SystemExit:
+        pass
